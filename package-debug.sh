a110b85ec940eed7c70cc29c9084a19fe3af4b2c
Simon Brown <simonbrown33@gmail.com>
2015-05-10 13:20:55 +0100

    updated package.sh to include .mem and created a debug and release version

---
+++ package-debug.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+cp Debug/webkit.js test/webkit.bin.js
+cp Debug/webkit.html.mem test/webkit.html.mem
+cp src/shells/demo.html test/demo.html
+cp src/webkit.api.js test/webkit.js
--- package.sh
+++ package.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
-cp Debug/webkit.js test/webkit.bin.js
+cp Release/webkit.js test/webkit.bin.js
+cp Release/webkit.html.mem test/webkit.html.mem
 cp src/shells/demo.html test/demo.html
 cp src/webkit.api.js test/webkit.js
