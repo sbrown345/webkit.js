4f31031e1baa91fbe770099092763176bfddc666
Simon Brown <simonbrown33@gmail.com>
2015-05-20 22:27:46 +0100

    package files into /bin

--- package.sh
+++ package.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
-mkdir test
-cp Release/webkit.js test/webkit.bin.js
-cp Release/webkit.html.mem test/webkit.html.mem
-cp src/shells/demo.html test/demo.html
-cp src/webkit.api.js test/webkit.js
+cp Release/webkit.js bin/webkit.bin.js
+cp Release/webkit.html.mem bin/webkit.html.mem
+cp src/shells/demo.html bin/demo.html
+cp src/webkit.api.js bin/webkit.js
